0072b9679c006e7b5583ad288cb1f3fc02f4a9f2
MaruvarKuzhali <muanvide3@gmail.com>
2022-07-27 17:16:44 +0530

    Initial Commit

--- config.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-context = CryptContext(
-        schemes=["pbkdf2_sha256"],
-        default="pbkdf2_sha256",
-        pbkdf2_sha256__default_rounds=50000
-)
-
-App_secret_key = "qrljuoezyihwmpxv"
-email_id = "neofisheries@gmail.com"
